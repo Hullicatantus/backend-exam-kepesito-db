8af3bb5940a1f0f5299216568c23e4f9081dab7b
Hullicatantus <nsl@freemail.hu>
2021-04-29 12:27:21 +0200

    This works

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,8 +1,6 @@
 package hu.nive.ujratervezes.jurassic;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,9 +20,21 @@ public class JurassicPark {
         return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
     }
 
-
     public <E> List<E> checkOverpopulation() {
         List <E> dinosaurNames = new ArrayList<>();
+
+        try(Connection connection = getConnection()){
+            String SQL = "SELECT breed FROM dinosaur WHERE actual > expected GROUP BY breed";
+
+            PreparedStatement statement = connection.prepareStatement(SQL);
+            ResultSet resultSet = statement.executeQuery();
+
+            while(resultSet.next()) {
+                dinosaurNames.add((E) resultSet.getString(1));
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
         return dinosaurNames;
     }
 }
